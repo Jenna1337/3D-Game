2aa3f8688f07fb0164201945fa461054b2002620
Jonah Sloan <jonah.m.sloan@gmail.com>
2015-12-09 14:14:20 -0600

    Started Input segments

---
+++ src/input/KeyInput.java
@@ -0,0 +1,30 @@
+package input;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter
+{
+	class KeyPressedAction
+	{
+
+	}
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
